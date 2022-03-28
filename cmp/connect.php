30f3cc79e7b281d9fec996855fd4743e7c88645e
Petra Christina <p.sedlaczek@drei.at>
2022-03-28 09:01:18 +0200

    Database connection

--- cmp/connect.php
+++ cmp/connect.php
@@ -1,14 +1,13 @@
 <?php
-
-$host = "localhost";
-$user = "root";
-$pwd = "";
-$db = "BE15_CR11_petadoption_Sedlaczek";
-
 // Create connection
-$connect = mysqli_connect($host, $user, $pwd, $db);
+$connect = mysqli_connect(
+  $host = "localhost",
+  $user = "root",
+  $pwd = "",
+  $db = "BE15_CR11_petadoption_Sedlaczek"
+);
 
 // Check connection
 if (!$connect) {
-  die("Connection failed: ".mysqli_connect_error());
+  die("Database connection failed: ".mysqli_connect_error());
 }
